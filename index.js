3b1bcc6963ee6f490779b9a4848bd006964fdd6d
dawood <dawood.altai111@gmail.com>
2023-04-06 00:54:01 +0300

    Complated assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
 
-// Write your code here
+let random = Math.ceil(Math.random() * 100);
+
+let num3=14
+let num4=5
+let mod =num3%num4;
+let max =(1,3,5,20);
